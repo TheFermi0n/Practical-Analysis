05b49a6da123986f2fbab0809385d3bd3ed91c79
Hanzo <63984668+okarin001@users.noreply.github.com>
2022-03-31 18:25:34 +0530

    all the packages I use usually

---
+++ setup.py
@@ -0,0 +1,95 @@
+"""
+Packages for plotting and other stuff
+version: 5.0
+author: Riasat
+"""
+# %matplotlib widget
+
+# data loading
+import pandas as pd
+
+# data maipulation
+import pwlf
+import numpy as np
+from scipy.interpolate import interp1d, UnivariateSpline
+from scipy.signal import find_peaks
+from scipy import optimize
+
+# plotting tools
+import matplotlib.pyplot as plt
+
+# extra tweaks
+import warnings
+
+warnings.filterwarnings("ignore")
+
+# plot tweaks
+plt.style.use("seaborn-poster")
+pd.options.display.max_columns = None
+pd.options.display.float_format = "{:.5f}".format
+
+# function for extrapolation
+def extrapolate1d(x, y):
+    f = interp1d(x, y, kind="linear", fill_value="extrapolate")
+    a = np.arange(0, x[len(x) - 1], 0.001)
+    b = f(a)
+    return a, b
+
+
+# function for interpolation
+def interpolate1d(x, y):
+    f = interp1d(x, y, kind="linear", fill_value="extrapolate")
+    a = np.arange(x[0], x[len(x) - 1], 0.001)
+    b = f(a)
+    return a, b
+
+
+# function for interpolation
+def interpolate2d(x, y):
+    f = interp1d(x, y, kind="quadratic", fill_value="extrapolate")
+    a = np.arange(x[0], x[len(x) - 1], 0.001)
+    b = f(a)
+    return a, b
+
+
+# function for interpolation
+def interpolate3d(x, y):
+    f = interp1d(x, y, kind="cubic", fill_value="extrapolate")
+    a = np.arange(x[0], x[len(x) - 1], 0.001)
+    b = f(a)
+    return a, b
+
+
+# funciton for polynomial fitting
+def polfit(a, b, c):
+    z = np.polyfit(a, b, c)
+    f = np.poly1d(z)
+
+    x = np.arange(a[0], a[len(a) - 1], 0.001)
+    y = f(x)
+    return x, y
+
+
+# function for picewise linear fit
+def picewise_linear_fit(x, y, segments):
+    my_pwlf = pwlf.PiecewiseLinFit(x, y)  # fit my data
+    res = my_pwlf.fit(segments)  # fit the data for n line segments
+    # slopes = myPWLF.calc_slopes()  # calculate slopes
+
+    # predict for the determined points
+    xHat = np.linspace(min(x), max(x), num=10000)
+    yHat = my_pwlf.predict(xHat)
+
+    # calculate statistics
+    # p = myPWLF.p_values(method="non-linear", step_size=1e-4)  # p-values
+    # se = myPWLF.se  # standard errors
+    return xHat, yHat
+
+
+# curve fit
+def cur_fit(x, y):
+    func = lambda t, a, c, d: a * np.log(t + c) + d
+    popt, pcov = optimize.curve_fit(func, x, y)  # type: ignore
+    xx = np.arange(x[0], x[len(x) - 1], 0.001)
+    yy = func(xx, *popt)
+    return xx, yy
